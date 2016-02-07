c5e4fd76eed26ae3165ef580e245747b28ac1d37
Nathaniel Kirby <nkirby.ps@gmail.com>
2016-02-06 23:28:04 -0500

    Better bridging to NSString for Linux

--- Sources/JSONValue.swift
+++ Sources/JSONValue.swift
@@ -12,10 +12,10 @@ public struct JSONValue<Element> {
     public init(value: Element?) {
         self.value = value
     }
-    
+
 // =======================================================
 // MARK: - Boxing
-    
+
     public func box<U>(@noescape block: ((JSONValue<Element>) -> U?)) -> JSONValue<U> {
         return JSONValue<U>(value: block(self))
     }
@@ -31,14 +31,33 @@ public struct JSONValue<Element> {
         return self.value as? Bool ?? defaultValue
     }
 
+#if os(Linux)
+    public func stringValue() -> String? {
+        //let string = self.value as? NSString
+        if let str = self.value as? NSString {
+                return String(str)
+        }
+        return nil
+    }
+#else
     public func stringValue() -> String? {
         return self.value as? String
     }
+#endif
 
+#if os(Linux)
+    public func stringValue(defaultValue defaultValue: String) -> String {
+        if let str = self.value as? NSString {
+            return String(str)
+        }
+        return defaultValue
+    }
+#else
     public func stringValue(defaultValue defaultValue: String) -> String {
         return self.value as? String ?? defaultValue
     }
-    
+#endif
+
     public func intValue() -> Int? {
         return self.value as? Int
     }
@@ -54,18 +73,18 @@ public struct JSONValue<Element> {
     public func floatValue(defaultValue defaultValue: Float = 0.0) -> Float {
         return self.value as? Float ?? defaultValue
     }
-    
+
     public func jsonDictionaryValue() -> JSONDictionary? {
         return self.value as? JSONDictionary
     }
-    
+
     public func jsonArrayValue() -> JSONArray? {
         return self.value as? JSONArray
     }
 
 // =======================================================
 // MARK: - Coercion
-    
+
     public func coerceString() -> String {
         if let str = self.value as? String {
             return str
@@ -75,14 +94,14 @@ public struct JSONValue<Element> {
             return "\(self.value)"
         }
     }
-    
+
     public func to<T>(type: T.Type) -> T? {
         return self.value as? T
     }
 
 // =======================================================
 // MARK: - Retreival
-        
+
     public subscript(index: String) -> JSONValue<AnyObject> {
         if let dict = self.value as? [String: AnyObject] {
             return JSONValue<AnyObject>(value: dict[index])
